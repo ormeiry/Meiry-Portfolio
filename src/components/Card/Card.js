68c71e20db2f4340d85d1ecdebaca85a6c8c4cb0
ormeiry <ormeiry2411@gmail.com>
2020-11-17 20:12:55 +0200

    Created a useObserver hook and used it across the project where needed.

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,43 +1,33 @@
-import React, { useEffect, useRef } from "react";
-import "./Card.css";
+import React from 'react';
+import './Card.css';
+
+import { useObserver } from '../../hooks/useObserver';
 
 const Card = ({ info: { title, listItems, link, github, name }, img }) => {
-  const ref = useRef();
-  useEffect(() => {
-    const observer = new IntersectionObserver(
-      ([entry]) => {
-        if (entry.isIntersecting) {
-          entry.target.classList.remove("unseen");
-          entry.target.classList.add("card");
-        }
-      },
-      {
-        threshold: [0.4],
-      }
-    );
-    if (ref.current) {
-      observer.observe(ref.current);
+  const options = { threshold: [0.4] };
+  const instructions = ([entry]) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.remove('unseen');
+      entry.target.classList.add('card');
     }
-    return () => {
-      observer.unobserve(ref.current);
-    };
-  }, []);
+  };
+  const observerRef = useObserver(instructions, options);
 
   const ul = listItems.map((item, i) => {
     return <li key={i}>{item}</li>;
   });
   return (
-    <div className="unseen" ref={ref}>
-      <img src={img} alt="project" className="project-image" />
-      <div className="project-links">
-        <a href={link} target="_blank" rel="noopener">
-          <i className="fas fa-eye fa-3x"></i>
+    <div className='unseen' ref={observerRef}>
+      <img src={img} alt='project' className='project-image' />
+      <div className='project-links'>
+        <a href={link} target='_blank' rel='noopener'>
+          <i className='fas fa-eye fa-3x'></i>
         </a>
-        <a href={github} target="_blank" rel="noopener">
-          <i className="fab fa-github fa-3x"></i>
+        <a href={github} target='_blank' rel='noopener'>
+          <i className='fab fa-github fa-3x'></i>
         </a>
       </div>
-      <div className="card-content">
+      <div className='card-content'>
         <h2>{name}</h2>
         <h4>{title}</h4>
         <ul>{ul}</ul>
