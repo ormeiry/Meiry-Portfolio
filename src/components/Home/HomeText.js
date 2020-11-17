50b204813b175d30b2101a66b16a6913e1079fd0
ormeiry <ormeiry2411@gmail.com>
2020-11-17 20:26:12 +0200

    Cleared and updated the master root for merging.

--- src/components/Home/HomeText.js
+++ src/components/Home/HomeText.js
@@ -1,31 +1,31 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect } from 'react';
 
-import "./HomeText.css";
+import './HomeText.css';
 
 const HomeText = ({ name, desc }) => {
-  const [nameText, setNameText] = useState("");
-  const [descText, setDescText] = useState("");
-  const [iconsClass, setIconsClass] = useState("unseen");
+  const [nameText, setNameText] = useState('');
+  const [descText, setDescText] = useState('');
+  const [iconsClass, setIconsClass] = useState('unseen');
 
   useEffect(() => {
     displayText(name, desc);
 
     return () => {
-      console.log("now");
-      setNameText("");
-      setDescText("");
+      console.log('now');
+      setNameText('');
+      setDescText('');
     };
   }, [name, desc]);
 
   useEffect(() => {
     if (descText.length === desc.length - 6) {
-      setIconsClass("icons-animation");
+      setIconsClass('icons-animation');
     }
   }, [descText, desc]);
 
   const displayText = (name, desc) => {
-    const nameArr = name.split("");
-    const descArr = desc.split("");
+    const nameArr = name.split('');
+    const descArr = desc.split('');
 
     for (let i = 0; i < nameArr.length; i++) {
       setTimeout(() => {
@@ -40,28 +40,28 @@ const HomeText = ({ name, desc }) => {
   };
 
   return (
-    <div className="home-text-container">
+    <div className='home-text-container'>
       <h1>{nameText}</h1>
       <p>{descText}</p>
       <div className={`icons`}>
         <a
           className={iconsClass}
-          href="https://www.linkedin.com/in/or-meiry-618b3719b/"
-          target="_blank"
-          rel="noopener"
+          href='https://www.linkedin.com/in/or-meiry-618b3719b/'
+          target='_blank'
+          rel='noopener'
         >
-          <i className="fab fa-linkedin fa-3x"></i>
+          <i className='fab fa-linkedin fa-3x'></i>
         </a>
         <a
           className={iconsClass}
-          href="https://github.com/ormeiry"
-          target="_blank"
-          rel="noopener"
+          href='https://github.com/ormeiry'
+          target='_blank'
+          rel='noopener'
         >
-          <i className="fab fa-github fa-3x"></i>
+          <i className='fab fa-github fa-3x'></i>
         </a>
-        <a className={iconsClass} href="mailto: ormeiry2411@gmail.com">
-          <i className="fa fa-envelope fa-3x" aria-hidden="true"></i>
+        <a className={iconsClass} href='mailto: ormeiry2411@gmail.com'>
+          <i className='fa fa-envelope fa-3x' aria-hidden='true'></i>
         </a>
       </div>
     </div>
