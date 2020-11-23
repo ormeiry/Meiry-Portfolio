54b9e268130703ef0de0220f93d304e590b268c3
ormeiry <ormeiry2411@gmail.com>
2020-11-23 18:43:14 +0200

    Fixed video loading time with a png placeholder for now.

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,16 +1,32 @@
-import React from "react";
+import React, { useEffect, useState } from 'react';
 
-import HomeText from "./HomeText";
-import bgVideo from "../../images/bgVideo.mp4";
-import "./Home.css";
+import HomeText from './HomeText';
+import bgVideo from '../../images/bgVideo.mp4';
+import bgStart from '../../images/bgStart.png';
+import './Home.css';
 
 const Home = () => {
+  const [bg, setBg] = useState(bgStart);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setBg(bgVideo);
+    setLoading(false);
+  }, []);
+
+  let content = loading ? (
+    <img src={bg} alt='bg' className='bg' />
+  ) : (
+    <video className='bg' autoPlay loop muted>
+      <source src={bg} type='video/mp4' />
+    </video>
+  );
+
+  console.log(content);
   return (
-    <div className="home-body">
-      <video className="video-tag" autoPlay loop muted>
-        <source src={bgVideo} type="video/mp4" />
-      </video>
-      <HomeText name="OR MEIRY" desc="Full Stack Developer" />
+    <div className='home-body'>
+      {content}
+      <HomeText name='OR MEIRY' desc='Full Stack Developer' />
     </div>
   );
 };
