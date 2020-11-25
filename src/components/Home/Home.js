8d5439fcfd1be18c24a6038f2969219a72146ffc
ormeiry <ormeiry2411@gmail.com>
2020-11-25 22:40:58 +0200

    Merged the new desigen and removed unused code.

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -7,20 +7,6 @@ import { ReactComponent as Logo } from '../../images/bgSVG.svg';
 import { ReactComponent as Wave } from '../../images/wave.svg';
 
 const Home = () => {
-  const [loading, setLoading] = useState(true);
-
-  // const changebgHandler = () => {
-  //   setLoading(false);
-  // };
-
-  // let content = loading ? (
-  //   <img src={bgStart} alt='bg' className='bg' />
-  // ) : (
-  //   <video className='bg' autoPlay loop muted>
-  //     <source src={bgVideo} type='video/mp4' />
-  //   </video>
-  // );
-
   return (
     <div className='home-body'>
       <Wave className='bg-wave' />
