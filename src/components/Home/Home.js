d6f08b5d07559ab032a13d6ae8dbf89ea42abf8e
Or Meiry <ormeiry2411@gmail.com>
2020-11-28 22:39:51 +0200

    Updated home text animations, Replaced font, updated the cards font size and the contact me styles.

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,17 +1,17 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
 
-import HomeText from './HomeText';
+import HomeText from "./HomeText";
 
-import './Home.css';
-import { ReactComponent as Logo } from '../../images/bgSVG.svg';
-import { ReactComponent as Wave } from '../../images/wave.svg';
+import "./Home.css";
+import { ReactComponent as Logo } from "../../images/bgSVG.svg";
+import { ReactComponent as Wave } from "../../images/wave.svg";
 
 const Home = () => {
   return (
-    <div className='home-body'>
-      <Wave className='bg-wave' />
-      <Logo className='bg' />
-      <HomeText name='OR MEIRY' desc='Full Stack Developer' />
+    <div className="home-body">
+      <Wave className="bg-wave" />
+      <Logo className="bg" />
+      <HomeText />
     </div>
   );
 };
--- src/components/Home/HomeText.js
+++ src/components/Home/HomeText.js
@@ -1,66 +1,56 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 
-import './HomeText.css';
+import "./HomeText.css";
 
 const HomeText = ({ name, desc }) => {
-  const [nameText, setNameText] = useState('');
-  const [descText, setDescText] = useState('');
-  const [iconsClass, setIconsClass] = useState('unseen');
+  // const [nameText, setNameText] = useState('');
+  // const [descText, setDescText] = useState('');
+  const [iconsClass, setIconsClass] = useState("unseen");
+
+  // useEffect(() => {
+  //   displayText(name, desc);
+
+  //   return () => {
+  //     setNameText('');
+  //     setDescText('');
+  //   };
+  // }, [name, desc]);
 
   useEffect(() => {
-    displayText(name, desc);
-
+    const timer = setTimeout(() => {
+      setIconsClass("icons-animation");
+    }, 500);
     return () => {
-      setNameText('');
-      setDescText('');
+      clearTimeout(timer);
     };
-  }, [name, desc]);
-
-  useEffect(() => {
-    if (descText.length === desc.length - 6) {
-      setIconsClass('icons-animation');
-    }
-  }, [descText, desc]);
-
-  const displayText = (name, desc) => {
-    const nameArr = name.split('');
-    const descArr = desc.split('');
-
-    for (let i = 0; i < nameArr.length; i++) {
-      setTimeout(() => {
-        setNameText((prev) => prev + nameArr[i]);
-      }, i * 200);
-    }
-    for (let i = 0; i < descArr.length; i++) {
-      setTimeout(() => {
-        setDescText((prev) => prev + descArr[i]);
-      }, i * 100 + nameArr.length * 250);
-    }
-  };
+  }, []);
 
   return (
-    <div className='home-text-container'>
-      <h1>{nameText}</h1>
-      <p>{descText}</p>
-      <div className={`icons`}>
+    <div className="home-text-container">
+      <h1>OR</h1>
+      <div className="flip">
+        <h1>MEIRY</h1>
+      </div>
+      <p>Full Stack Developer</p>
+      <div className="icons">
         <a
           className={iconsClass}
-          href='https://www.linkedin.com/in/or-meiry-618b3719b/'
-          target='_blank'
-          rel='noopener'
+          href="https://www.linkedin.com/in/or-meiry-618b3719b/"
+          target="_blank"
+          rel="noopener"
         >
-          <i className='fab fa-linkedin fa-3x'></i>
+          <i className="fab fa-linkedin fa-3x"></i>
         </a>
         <a
           className={iconsClass}
-          href='https://github.com/ormeiry'
-          target='_blank'
-          rel='noopener'
+          href="https://github.com/ormeiry"
+          target="_blank"
+          rel="noopener"
         >
-          <i className='fab fa-github fa-3x'></i>
+          <i className="fab fa-github fa-3x"></i>
         </a>
-        <a className={iconsClass} href='mailto: ormeiry2411@gmail.com'>
-          <i className='fa fa-envelope fa-3x' aria-hidden='true'></i>
+        <a className={iconsClass} href="mailto: ormeiry2411@gmail.com">
+          <i className="fa fa-envelope fa-3x" aria-hidden="true"></i>
         </a>
       </div>
     </div>
