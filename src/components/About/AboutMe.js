4a08ae7749a15047c1175b37388fb19abd7b0730
ormeiry <ormeiry2411@gmail.com>
2021-01-24 05:22:51 +0200

    Updated the about me content.

--- src/components/About/AboutMe.js
+++ src/components/About/AboutMe.js
@@ -1,9 +1,9 @@
-import React from "react";
-import "./AboutMe.css";
+import React from 'react';
+import './AboutMe.css';
 
-import { useObserver } from "../../hooks/useObserver";
-import { ReactComponent as Wave } from "../../images/aboutwave.svg";
-import { ReactComponent as AboutMeSVG } from "../../images/aboutmeSVG.svg";
+import { useObserver } from '../../hooks/useObserver';
+import { ReactComponent as Wave } from '../../images/aboutwave.svg';
+import { ReactComponent as AboutMeSVG } from '../../images/aboutmeSVG.svg';
 
 const AboutMe = () => {
   // const instructions = ([entry]) => {
@@ -17,19 +17,19 @@ const AboutMe = () => {
   // const options = { threshold: [0.1] };
   // const observerRef = useObserver(instructions, options);
   return (
-    <div className="about-container">
-      <Wave className="about-wave" />
-      <AboutMeSVG className="about-me-svg" />
-      <h2 className="title">
+    <div className='about-container'>
+      <Wave className='about-wave' />
+      <AboutMeSVG className='about-me-svg' />
+      <h2 className='title'>
         <span>A</span>bout Me
       </h2>
       <p>
         I love People, Programming, and learning new things. Currently looking
         for a place that has all three. I have dedicated my time and effort to
         turn my life upside down, stopped being scared of failing, and jumped
-        headfirst into this amazing field. I finished over 240 hours of
+        headfirst into this amazing field. I finished over 270 hours of
         professional online courses, and far more of them writing, reading, and
-        understanding code, My journey continues to this day. The hunger for
+        understanding code. My journey continues to this day. The hunger for
         knowledge is growing as I learn more. All I need is a place where I can
         grow as a developer, and surly enough my contribution will come fast.
       </p>
